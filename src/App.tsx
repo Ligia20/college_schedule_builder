2a703dad9b2d10c3cda65e694e5773c82bca33be
Ligia <ligiaarevalo251@gmail.com>
2026-06-10 16:47:55 -0400

    added a new box that can overlap the schedule, however there are issues. Need to add the function of courses too

--- src/App.tsx
+++ src/App.tsx
@@ -21,10 +21,19 @@ interface CalenderWeek {
 */
 export default function App() {
 
+        const totalDays = 7
+        const columnWidthPercent = 100 / totalDays;
+        const rowHeightPx = 57;
     
     //const [courses, setCourses] = useState([])
+    const eventSettings = {
+        left: `calc(70px + (${2 * columnWidthPercent}%))`,
+        width: `calc(${2* columnWidthPercent}%)`,
+        top: `${1 * rowHeightPx}px`,
+        height: `${3 * rowHeightPx}px`,
+    };
     
-    const dayLabels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const dayLabels = [' ', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     /*const [messages, setMessages] = useState<string[]>([]); */
     const timeSlots = ['9:00 AM', '10:00 AM','11:00 AM','12:00 PM','1:00 PM',
         '2:00 PM','3:00 PM','4:00 PM','5:00 PM','6:00 PM','7:00 PM','8:00 PM',
@@ -38,10 +47,6 @@ export default function App() {
         )
  
     */}
-    
-    <div class="wrapper">
-        <div>one</div>
-    </div>
 
     <div className="calender_container">
         <div className="week_grid">
@@ -51,20 +56,25 @@ export default function App() {
                 </div>
             ))}
         </div>
-    </div>
-    <div className="schedule_body">
+        <div className="schedule_body">
         {timeSlots.map((time, timeIndex) => (
-            <div key={timeIndex} className="schedule_grid_row">
+            <div key={timeIndex} className="schedule_grid_row" style={{height: `${rowHeightPx}`}}>
                 <div className="time_label_cell">
                     {time}
                 </div>
-                {dayLabels.map((_,dayIndex)=> (
+            
+                {dayLabels.map((_,dayIndex) => (
                     <div key={dayIndex} className="time_slot_box">
                     </div>
                 ))}
             </div>
-        ))}
+        ))} 
+            <div className="wrapper" style={eventSettings}>
+                <div>one</div>
+            </div>
+        </div>
     </div>
+        
     {/* <h1 className="CSBtitle">College Schedule Builder</h1> */}
     </>;
 
