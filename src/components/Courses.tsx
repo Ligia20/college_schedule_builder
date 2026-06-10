077d8e17f0d7b190bdba5dd1f30987e83b9de0a2
Ligia <ligiaarevalo251@gmail.com>
2026-06-10 13:59:11 -0400

    created the calender in the color pink

---
+++ src/components/Courses.tsx
@@ -0,0 +1,8 @@
+
+    /*const Courses = ({text, index}: {text:string, index: number}) => {
+        return <>
+        <p className = 'courseinfo'>
+            {text}
+        </p>
+        </>
+    }*/
